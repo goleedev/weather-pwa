ab444ba44f83a07e136f60e8f7c125ee47ead6e0
GO Lee <golee.dev@gmail.com>
2020-05-30 22:19:09 +0900

    PWA using react and open weather api

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -14,7 +14,28 @@ self.addEventListener("install", event => {
 });
 
 // Listen for requests
-self.addEventListener("fetch", event => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
 
 // Activate SW
-self.addEventListener("activate", event => {});
+self.addEventListener("activate", event => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhiteList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
